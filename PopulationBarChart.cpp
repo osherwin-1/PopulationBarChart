d54521d0b04d60884f211aac321df95a802f7886
osherwin-1 <olive@DESKTOP-7OL15UE>
2025-11-12 22:24:50 -0500

    Final

--- PopulationBarChart.cpp
+++ PopulationBarChart.cpp
@@ -5,27 +5,33 @@
 #include <fstream>
 using namespace std;
 int main()
-{
+{ //This program creates a bar graph based on population data from a file.
 	string town;
-	cout << "Enter the name of the town: ";
+	cout << "Enter the name of the town: "; //User inputs town name
 	cin >> town;
 	string dataFile;
-	cout << "Enter the name of the data file: ";
+	cout << "Enter the name of the data file (Include file extension): "; //User inputs file name
 	cin >> dataFile;
 	ifstream inputFile(dataFile);
 	if (!inputFile) {
 		cout << "Unable to open file.";
-		return 1;
+		return 1; //Code ends if file does not open
 	}
 	int population;
-	while (inputFile >> population) {
-		cout << town << "Population growth" << endl;
-		cout << "Each * represents 1000 people" << endl;
+	int year = 1900;
+	//Headers for bar graph
+	cout << town << " Population growth" << endl;
+	cout << "Each * represents 1000 people" << endl;
+	while (inputFile >> population) { //Reads data from file and then displays a star for each 1000 people
+		cout << year << " ";
+		year += 20;
 		int barLength = population / 1000;
 		for (int i = 0; i < barLength; ++i) {
 			cout << "*";
 		}
-		cout << " " << population << endl;
+		cout << endl;
+		
 	}
-
+	inputFile.close(); //Make sure to close the file!
+	return 0;
 }
